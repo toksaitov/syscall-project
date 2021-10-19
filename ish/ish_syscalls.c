2ef311df5919aa3f7608c651258c1ed9a4c9799e
Sergei Rachmiroff <rachmiroff@gmail.com>
2021-10-19 11:10:53 +0600

    Remove info about old architectures

--- ish/ish_syscalls.c
+++ ish/ish_syscalls.c
@@ -3,15 +3,6 @@
 /*
     Function Calling Convention
 
-    On x86 (Linux, BSD, Mac OS X) arguments are passed on the stack and pushed
-    from right to left.
-
-        buffer_size is pushed first
-        the buffer pointer is pushed next
-        file_descriptor is pushed last
-
-    Result is returned in the eax register.
-
     On x86-64 (Linux, BSD, Mac OS X) the first six integer or pointer
     arguments are passed in registers rdi, rsi, rdx, rcx, r8, and r9.
 
@@ -20,16 +11,6 @@
        buffer_size goes to rdx
 
     Result is returned in the rax register.
-
-    On ARM (A32) (Linux, BSD, Mac OS X) the first four integer or pointer
-    arguments are passed in registers r0 through r3. The same registers are
-    used to return values.
-
-       file_descriptor goes to r0
-       the buffer pointer goes to r1
-       buffer_size goes to r2
-
-    Result is returned in the r0 register.
 
     On ARM (A64) (Linux, BSD, Mac OS X) the first eight integer or pointer
     arguments are passed in registers x0 through x7. The same registers are
@@ -40,9 +21,6 @@
        buffer_size goes to x2
 
     Result is returned in the x0 register.
-
-    You will have to find the calling conventions for MIPS and RISC-V on your
-    own.
 
     ---
     Kernel Calling Convention
