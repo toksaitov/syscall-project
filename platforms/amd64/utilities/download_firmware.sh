2036a649ecec308d3d850d01bbe3429a29f4eab5
Dmitrii Toksaitov <dmitrii@toksaitov.com>
2023-10-29 15:07:08 +0600

    Update amd64 OVMF firmware to 2023.05-2

--- platforms/amd64/utilities/download_firmware.sh
+++ platforms/amd64/utilities/download_firmware.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/env sh
 
 FIRMWARE_ARCHIVE='firmware-x64.tar.gz'
-EFI_FIRMWARE='OVMF.fd'
+EFI_FIRMWARE_CODE='OVMF_CODE_4M.fd'
+EFI_FIRMWARE_VARS='OVMF_VARS_4M.fd'
 LINUX_KERNEL='vmlinuz'
 LINUX_RAMDISK='initrd.gz'
 
 URL="https://www.dropbox.com/scl/fi/3vjcx3wsxaoncvjgm794k/$FIRMWARE_ARCHIVE?rlkey=pj3l6mvch867gmh2plzioa38b&dl=1"
 curl --location -C - --output "$FIRMWARE_ARCHIVE" "$URL"
 
-test -e "$EFI_FIRMWARE"  && mv "$EFI_FIRMWARE"  "$EFI_FIRMWARE".old
+test -e "$EFI_FIRMWARE_CODE" && mv "$EFI_FIRMWARE_CODE" "$EFI_FIRMWARE_CODE".old
+test -e "$EFI_FIRMWARE_VARS" && mv "$EFI_FIRMWARE_VARS" "$EFI_FIRMWARE_VARS".old
 test -e "$LINUX_KERNEL"  && mv "$LINUX_KERNEL"  "$LINUX_KERNEL".old
 test -e "$LINUX_RAMDISK" && mv "$LINUX_RAMDISK" "$LINUX_RAMDISK".old
 tar -xvzf "$FIRMWARE_ARCHIVE"
--- platforms/amd64/utilities/remove_old_files.sh
+++ platforms/amd64/utilities/remove_old_files.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env sh
 
 DISK_IMAGE='debian-12.1.0-amd64-hd.qcow2'
-EFI_FIRMWARE='OVMF.fd'
+EFI_FIRMWARE_CODE='OVMF_CODE_4M.fd'
+EFI_FIRMWARE_VARS='OVMF_VARS_4M.fd'
 LINUX_KERNEL='vmlinuz'
 LINUX_RAMDISK='initrd.gz'
 
-rm -i "$DISK_IMAGE".old    \
-      "$EFI_FIRMWARE".old  \
-      "$LINUX_KERNEL".old  \
+rm -i "$DISK_IMAGE".old        \
+      "$EFI_FIRMWARE_CODE".old \
+      "$EFI_FIRMWARE_VARS".old \
+      "$LINUX_KERNEL".old      \
       "$LINUX_RAMDISK".old
